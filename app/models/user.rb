bc57638753e2b0bd44e1618977515d101eef0f76
Takemura <takemura@mba.local>
2017-02-14 15:58:35 +0900

    Twitterログイン　テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,43 +7,37 @@ class User < ActiveRecord::Base
   has_many :blogs
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
-    # user= User.find_by(email: auth.info.email)
-    # unless user
-    #   user=User.new(
-    #     name:     auth.extra.raw_info.name,
-    #     provider: auth.provider,
-    #     uid:      auth.uid,
-    #     email:    auth.info.email ||="#{auth.uid}-#{auth.provider}@example.com",
-    #     image_url:auth.info.image,
-    #     password: Devise.friendly_token[0,20]
-    #   )
-    #   user.skip_confirmation!
-    #   user.save(validate:false)
-    # end
-    # user
     user= User.find_by(email: auth.info.email)
-    set_auth_user_infomation(auth,user)
+    unless user
+      user=User.new(
+        name:     auth.extra.raw_info.name,
+        provider: auth.provider,
+        uid:      auth.uid,
+        email:    auth.info.email ||="#{auth.uid}-#{auth.provider}@example.com",
+        image_url:auth.info.image,
+        password: Devise.friendly_token[0,20]
+      )
+      user.skip_confirmation!
+      user.save(validate:false)
+    end
+    user
   end
 
   def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
     user= User.find_by(privider: auth.provider, uid: auth.uid)
-    set_auth_user_infomation(auth,user)
+    unless user
+      user=User.new(
+        name:     auth.extra.raw_info.name,
+        provider: auth.provider,
+        uid:      auth.uid,
+        email:    auth.info.email ||="#{auth.uid}-#{auth.provider}@example.com",
+        image_url:auth.info.image,
+        password: Devise.friendly_token[0,20]
+      )
+      user.skip_confirmation!
+      user.save(validate:false)
+    end
+    user
   end
 
-  private
-    def set_auth_user_infomation(auth,user)
-      unless user
-        user=User.new(
-          name:     auth.extra.raw_info.name,
-          provider: auth.provider,
-          uid:      auth.uid,
-          email:    auth.info.email ||="#{auth.uid}-#{auth.provider}@example.com",
-          image_url:auth.info.image,
-          password: Devise.friendly_token[0,20]
-        )
-        user.skip_confirmation!
-        user.save(validate:false)
-      end
-      user
-    end
 end
