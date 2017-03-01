f8a06378b6c9a7c98607f1d1881a49af983f95a4
Takemura <takemura@mba.local>
2017-03-01 15:36:05 +0900

    follow

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,10 @@ Rails.application.routes.draw do
 
   resources :poems, only: [:index, :show]
 
+  resources :users, only: [:index, :show]
+
+  resources :relationships, only: [:create, :destroy]
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at:"/letter_opener"
   end
