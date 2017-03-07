dba31c802baad85b46a23043d88e4393ee4fe9de
Takemura <takemura@mba.local>
2017-03-07 18:24:03 +0900

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: 'https://54.65.250.140/'}
+  config.action_mailer.default_url_options = {host: 'http://54.65.250.140'}
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
